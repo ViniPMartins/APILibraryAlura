bfe849c04c01bcc08125a96898493b710938b3a3
Vinícius Martins <vinicius.pereira.mts@gmail.com>
2024-02-19 11:44:51 +0000

    Inserindo middleware manipulador de erros

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js"
 import routes from "./routes/index.js"
+import manipuladorErros from "./middlewares/manipuladorErros.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
@@ -8,7 +9,8 @@ db.once("open", () => {
 })
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 routes(app);
+app.use(manipuladorErros)
 
 export default app
